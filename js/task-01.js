23340016500c4d3412ccea3a67ac4f79c87535fd
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-06 13:07:51 +0100

    del comments

--- js/task-01.js
+++ js/task-01.js
@@ -2,14 +2,10 @@ const categories = document.querySelector('#categories');
 const noOfCategories = Number(categories.children.length);
 console.log(`Number of categories: ${noOfCategories}`);
 console.log('');
-// console.dir(categories.children);
-// const title = document.querySelector('.title');
-// console.dir(title);
 
 [...categories.children].forEach((element) => {
     const categoryName = element.children[0].textContent;
     console.log(`Category: ${categoryName}`);
-    // console.dir(element.children[0].textContent);
     console.log(`Elements: ${element.children[1].children.length}`);
     console.log(' ');
 });
