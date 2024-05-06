12ac23a761381352de73f9300d6a68adaaa0304b
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-06 14:33:07 +0100

    cleanup

--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,12 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
 const list = document.querySelector('#ingredients');
-const ingredientList = document.createDocumentFragment();
+const fragment = document.createDocumentFragment();
 
 ingredients.forEach((ingredient) => {
     const listItem = document.createElement('li');
     listItem.textContent = ingredient;
     listItem.classList.add('item');
-    ingredientList.append(listItem);
+    fragment.appendChild(listItem);
 });
-list.appendChild(ingredientList);
+list.appendChild(fragment);
