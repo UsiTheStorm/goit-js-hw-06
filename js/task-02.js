a0a3c72c8f7d28ac81b3cac8c705c66db5d98646
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-06 14:08:08 +0100

    vol 1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+// let ingredientList;
+const list = document.querySelector('#ingredients');
+
+ingredients.forEach((ingredient) => {
+    const listItem = document.createElement('li');
+    // console.log(listItem);
+    listItem.textContent = ingredient;
+    listItem.classList.add('item');
+    // ingredientList += listItem;
+    // console.log(listItem);
+    list.append(listItem);
+});
+// console.log(ingredientList);
