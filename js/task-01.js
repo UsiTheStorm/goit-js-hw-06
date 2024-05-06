1bc3f619e77c21247938ddc2702d015b8379230c
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-06 13:07:25 +0100

    refactoring

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,15 @@
-const cat = document.querySelector('#categories');
-console.dir(cat.children.length);
-console.dir(cat.children);
+const categories = document.querySelector('#categories');
+const noOfCategories = Number(categories.children.length);
+console.log(`Number of categories: ${noOfCategories}`);
+console.log('');
+// console.dir(categories.children);
 // const title = document.querySelector('.title');
 // console.dir(title);
 
-[...cat.children].forEach((element) => {
-    console.dir(element.children[0].textContent);
-    console.dir(element.children[1].children.length);
+[...categories.children].forEach((element) => {
+    const categoryName = element.children[0].textContent;
+    console.log(`Category: ${categoryName}`);
+    // console.dir(element.children[0].textContent);
+    console.log(`Elements: ${element.children[1].children.length}`);
+    console.log(' ');
 });
