d7dc8119390d3c28bcf303b49a9d9b1fb6495735
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-08 14:15:05 +0100

    v2

--- js/task-07.js
+++ js/task-07.js
@@ -2,9 +2,8 @@ const fontSizer = document.getElementById('font-size-control');
 const text = document.getElementById('text');
 
 function sizeChange(event) {
-    console.dir(event.currentTarget.value);
     const size = event.currentTarget.value;
     text.style.fontSize = `${size}px`;
 }
 
-fontSizer.addEventListener('change', sizeChange);
+fontSizer.addEventListener('input', sizeChange);
