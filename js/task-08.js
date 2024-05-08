9d18c82a9b09a196477e9ca0dcbc3b582e2a4e21
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-08 15:16:51 +0100

    v2

--- js/task-08.js
+++ js/task-08.js
@@ -2,15 +2,10 @@ const form = document.querySelector('.login-form');
 
 function submitForm(event) {
     event.preventDefault();
-    console.dir(event.currentTarget.elements.email.value);
-    console.dir(event.currentTarget);
-
-    // const email = event.currentTarget.elements.email.value;
-    // const password = event.currentTarget.elements.password.value;
 
     const { email, password } = event.currentTarget.elements;
     if (!email.value || !password.value) {
-        document.insertAdjacentHTML('beforeend', 'alert("Complete All Fields")');
+        alert('Complete All Fields');
     } else {
         const data = {
             email: email.value,
