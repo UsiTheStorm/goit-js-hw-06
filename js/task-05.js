ff04a5f082b2d22be21ee22af53e97dc461492f5
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-08 12:15:05 +0100

    v2

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,16 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
+function capitalizeName(name) {
+    return name[0].toUpperCase() + name.slice(1).toLowerCase();
+}
+
 function onInputChange(event) {
     const inputValue = event.currentTarget.value;
     if (inputValue === '') {
         output.textContent = 'Anonymous';
     } else {
-        output.textContent = inputValue;
+        output.textContent = capitalizeName(inputValue);
     }
 }
 
