b8d02ca46100f5e86e1d7cb23cc9702ee5fc744a
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-09 11:26:02 +0100

    v1

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+btn.addEventListener('click', changeColor);
