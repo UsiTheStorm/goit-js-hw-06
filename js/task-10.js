a548ed7258f20068288695765bf60a221e24b75c
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-09 12:11:14 +0100

    v1

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,34 @@
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const input = document.querySelector('input');
+const boxSpace = document.querySelector('#boxes');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+function createBoxes() {
+    const amount = input.value;
+    const divs = [];
+    console.dir(input);
+    for (let i = 0; i < amount; i += 1) {
+        const box = document.createElement('div');
+        box.style.width = `${30 + 10 * i}px`;
+        box.style.height = `${30 + 10 * i}px`;
+        box.style.backgroundColor = getRandomHexColor();
+        console.log(box);
+        divs.push(box);
+    }
+    boxSpace.append(...divs);
+    // console.log(divs);
+}
+
+function destroyBoxes() {
+    input.value = '';
+    boxSpace.innerHTML = '';
+}
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
